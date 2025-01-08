33900216b93064aba0e03411c3c41af586e1d3c6
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-01-08 15:10:46 -0600

    mock and API from external file

---
+++ src/test/java/restAssuresTest/Places.java
@@ -0,0 +1,32 @@
+package restAssuresTest;
+
+import static io.restassured.RestAssured.given;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import files.ReusableMethods;
+import files.payload;
+import io.restassured.RestAssured;
+
+public class Places {
+
+    @Test
+    public void addPlace() throws IOException{
+        RestAssured.baseURI = "https://rahulshettyacademy.com/";
+
+        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+        .body(new String (Files.readAllBytes(Paths.get("src\\main\\resources\\addPlace.json"))))
+        .when().post("maps/api/place/add/json")
+        .then().log().all().assertThat().statusCode(200)
+        .body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)").extract().response()
+        .asString();
+
+          String placeId = ReusableMethods.rawToString(response, "place_id");
+
+    }
+
+}
