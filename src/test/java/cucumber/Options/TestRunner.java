b8b9a3d9eec7a9f5d89fef10d078f30f6b6feda3
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-01-20 20:44:15 -0600

    add reporting

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -8,7 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features="src\\test\\java\\features",
     glue = {"stepDefinitions"},
-    tags ="@DeletePlace")
+    plugin = "json:target//jsonReports//cucumber-report.json")
+    //tags ="@DeletePlace")
 public class TestRunner {
 
 }
