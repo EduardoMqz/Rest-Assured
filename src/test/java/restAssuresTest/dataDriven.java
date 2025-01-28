fb81bfa7ca82c9b99672a3d72229411259432a74
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-01-27 21:29:15 -0600

    iterate rows

--- src/test/java/restAssuresTest/dataDriven.java
+++ src/test/java/restAssuresTest/dataDriven.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import files.ReusableMethods;
 
 public class dataDriven {
-    
+
     ReusableMethods configReader = new ReusableMethods("excelResource");
 
     @Test
@@ -25,10 +25,22 @@ public class dataDriven {
                 Iterator<Row> rows = sheet.iterator();// collection of rows
                 Row firstRow = rows.next();
                 Iterator<Cell> cells = firstRow.cellIterator();// collection of cells
+                int k = 0;
+                int column = 0;
                 while (cells.hasNext()) {
                     Cell cell = cells.next();
                     if (cell.getStringCellValue().equalsIgnoreCase(configReader.get("cellValue"))) {
-
+                        column = k;
+                    }
+                    k++;
+                }
+                while(rows.hasNext()){
+                    Row row =rows.next();
+                    if(row.getCell(column).getStringCellValue().equalsIgnoreCase(configReader.get("value"))){
+                        Iterator<Cell> cv = row.cellIterator();
+                        while (cv.hasNext()) {
+                            System.out.println(cv.next().getRichStringCellValue());  
+                        }
                     }
                 }
             }
